583eb5e2bd3c93fad52c5cd05b74c76b562cff26
Me <ghost_vision@abv.bg>
2014-03-18 15:32:26 +0200

    this works

---
+++ show_lists.py
@@ -0,0 +1,16 @@
+import os
+
+
+class ShowLists:
+    @staticmethod
+    def show_lists():
+        directory_content = os.listdir()
+        files = []
+        length = 0
+        for i in directory_content:
+            if ( (".txt" in i) and ("[" in i ) ):
+                length = len(i)
+                files.append(i[:length-4])
+        return files
+
+
