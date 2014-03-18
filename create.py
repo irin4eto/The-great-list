933467c816a7f8c6dc17aae31a244a9765adcbfd
reachh <irina.bs@abv.bg>
2014-03-18 15:56:57 +0200

    Adding create.py and test_create.py

---
+++ create.py
@@ -0,0 +1,11 @@
+import show_lists
+
+class Create():
+    def __init__(self, list_name):
+        self.next_number = len(show_lists.ShowLists.show_lists())
+        self.list_name = "[{}] list_name{}".format(self.next_number, ".txt")
+
+    def create(self):
+        file = open(self.list_name, "w")
+        file.close()
+
