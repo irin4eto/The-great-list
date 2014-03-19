d9ca8b14de658a7c3d522cfb20ce0f5a7ae4d4d7
reachh <irina.bs@abv.bg>
2014-03-19 16:09:34 +0200

    Changing test_create.py

--- test_create.py
+++ test_create.py
@@ -6,14 +6,13 @@ import os
 
 
 class TestCreate(unittest.TestCase):
-    def setUp(self):
-        self.c = create.Create("HackBulgaria")
 
     def test_create(self):
+        self.c = create.Create("HackBulgaria")
+
         self.c.create()
         self.assertEqual(True, os.path.isfile(self.c.list_name))
 
-    def tearDown(self):
         os.remove(self.c.list_name)
 
 if __name__ == '__main__':
