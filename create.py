eaa3001b1b4897dbfbd37670572ab875675642a4
reachh <irina.bs@abv.bg>
2014-03-20 15:48:41 +0200

    Changing create.py

--- create.py
+++ create.py
@@ -3,7 +3,7 @@ import show_lists
 class Create():
     def __init__(self, list_name):
         self.next_number = len(show_lists.ShowLists.show_lists()) + 1
-        self.list_name = "[{}] list_name{}".format(self.next_number, ".txt")
+        self.list_name = "[{}] {}{}".format(self.next_number, list_name, ".txt")
 
     def create(self):
         file = open(self.list_name, "w")
